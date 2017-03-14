f5eaf9edaad6fbc3a8799a7d40d1ed4e804172f9
lshao <laetitia.shao@gmail.com>
2017-03-14 19:02:17 +0100

    Last commit

---
+++ build/CMakeFiles/images.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/images.dir/images.cpp.o"
+  "CMakeFiles/images.dir/polygon.cpp.o"
+  "CMakeFiles/images.dir/data.cpp.o"
+  "images.pdb"
+  "images"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/images.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
